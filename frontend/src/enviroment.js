f52b1876d5f47f609ff91ed885c25d2b043dcc7a
Praveen kumar <praveenkumarprakash8@gmail.com>
2026-07-20 20:24:06 +0530

    Update backend server URL

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
+  ? "https://apnacollegebackend-2vs5.onrender.com"
   : "http://localhost:8000";
 
 export default server;
